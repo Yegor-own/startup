5226eedfc2655ac81871fc57d7f4fd76d98b5fe3
Yegor-own <egor891712@gmail.com>
2022-02-19 19:55:28 +0500

    add new section footer

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 		$('.header__burger,.header__list').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	$(window).scroll(function() {
+		var target = $(this).scrollTop();
+		if(target == 0) {
+			$('.header__nav').removeClass('scroll');
+		} else {
+			$('.header__nav').addClass('scroll');
+		}
+	});
 });
