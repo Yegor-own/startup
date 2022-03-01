8546905b5e7db5aec9b554b41d401e973b95e712
Yegor-own <egor891712@gmail.com>
2022-03-01 17:27:18 +0500

    add new section gallery

--- docs/js/app.js
+++ docs/js/app.js
@@ -15,14 +15,26 @@ $(document).ready(function() {
 
 	let windowInnerWidth = window.innerWidth;
 	let slides = 4;
-	if (windowInnerWidth <= 768) {
-		slides = 2;
-		if (windowInnerWidth <= 425) {
-			slides = 1;
+	if (windowInnerWidth <= 1024) {
+		slides = 3;
+		if (windowInnerWidth <= 768) {
+			slides = 2;
+			if (windowInnerWidth <= 647) {
+				slides = 1;
+			}
 		}
 	}
 	$('.about-us__slider').slick({
 		slidesToShow: slides,
 		infinite: false,
 	});
+
+	function ibg() {
+		$.each($('.ibg'), function(index, val) {
+			if($(this).find('img').length>0){
+				$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+			}
+		});
+	}
+	ibg();
 });
